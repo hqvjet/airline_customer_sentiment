423e59a124e47a7909ecc76877ad84315ae0173c
hqvjet <viethq.1906@gmail.com>
2024-01-17 15:19:26 +0700

    imp test model

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
     rating = getRatingFromModel(req.get('title'), req.get('text'))
     return rating
 
-app.run(host='113.160.235.186', port=5423, debug=False)
+app.run(host='0.0.0.0', port=8081, debug=False)
---
+++ testModel.py
@@ -0,0 +1,5 @@
+from loadModel import getModel
+from appService import getRatingFromModel
+
+model = getModel()
+a = getRatingFromModel('Trên cả tuyệt vời', 'Khách sạn này thực sự quá là tuyệt vời, trên cả mong đợi')
