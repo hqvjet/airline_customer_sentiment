3b8f8fcf96bf9fc56a67c117f0d6f3c56244c26a
hqvjet <viethq.1906@gmail.com>
2024-01-18 10:03:54 +0700

    update

--- testModel.py
+++ testModel.py
@@ -1,5 +1,8 @@
 from loadModel import getModel
 from appService import getRatingFromModel
 
+print('LOADING MODEL..........................')
 model = getModel()
 a = getRatingFromModel('Trên cả tuyệt vời', 'Khách sạn này thực sự quá là tuyệt vời, trên cả mong đợi')
+print('Donee.........................')
+print(a)
