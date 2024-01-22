c28c1fbffa6224c4dc1b84d687d285f981602690
hqvjet <viethq.1906@gmail.com>
2024-01-22 09:02:57 +0700

    ready to deploy, tuning model ouput

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from appService import getRatingFromModel
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['POST'])
 def index():
     req = request.get_json()
     rating = getRatingFromModel(req.get('title'), req.get('text'))
-    return rating
+    data = rating.tolist()
+    return jsonify({'data': data})
 
 app.run(host='0.0.0.0', port=8081, debug=False)
