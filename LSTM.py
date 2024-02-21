14b925a7620cee23aaade64f4853ec7ceb403104
hqvjet <viethq.1906@gmail.com>
2024-02-21 19:45:25 +0700

    update

--- LSTM.py
+++ LSTM.py
@@ -1,12 +1,15 @@
-from keras.models import Model
+from keras.models import Model, load_model
+from keras import regularizers
 from tensorflow.keras.layers import Embedding
-from keras.layers import Input, Embedding, LSTM as LSTM_model, Dense, Average
+from keras.layers import Input, Embedding, LSTM as LSTM_model, Dropout, Dense, concatenate, Average
 from tensorflow.keras import utils
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 import numpy as np
-from keras.utils import plot_model
-import matplotlib.pyplot as plt
 from constants import *
+from keras.utils import plot_model
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 class LSTM:
@@ -37,19 +40,24 @@ class LSTM:
 
     def getOutput(self):
         hidden_size = 256
+        DROP = 0.3
 
         self.title_input = Input(shape=(self.train_title.shape[1],))
-        title_embedding = Embedding(self.vocab_size, EMBEDDING_DIM, weights=[self.embedding_matrix], input_length=self.train_title.shape[1], trainable=False)(self.title_input)
+        title_embedding = Embedding(self.vocab_size, EMBEDDING_DIM, input_length=self.train_title.shape[1], weights=[self.embedding_matrix], trainable=TRAINABLE)(self.title_input)
         title_lstm = LSTM_model(hidden_size)(title_embedding)
+        title_lstm = Dropout(DROP)(title_lstm)
 
         self.text_input = Input(shape=(self.train_text.shape[1],))
-        text_embedding = Embedding(self.vocab_size, EMBEDDING_DIM, weights=[self.embedding_matrix], input_length=self.train_text.shape[1], trainable=False)(self.text_input)
+        text_embedding = Embedding(self.vocab_size, EMBEDDING_DIM, input_length=self.train_text.shape[1], weights=[self.embedding_matrix], trainable=TRAINABLE)(self.text_input)
         text_lstm = LSTM_model(hidden_size)(text_embedding)
+        text_lstm = Dropout(DROP)(text_lstm)
 
         average = Average()([title_lstm, text_lstm])
 
-        # dense1 = Dense(512, activation='relu')(combined)
-        return Dense(self.train_rating.shape[1], activation='softmax')(average)
+        final = Dense(200, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001))(average)
+        final = Dropout(DROP)(final)
+
+        return Dense(3, activation='softmax')(final)
 
     def buildModel(self):
 
@@ -58,27 +66,30 @@ class LSTM:
 
         model_LSTM.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         model_LSTM.summary()
-        plot_model(model_LSTM, to_file=PATH + MODEL_IMAGE + LSTM_IMAGE, show_shapes=True, show_layer_names=True)
         
+        plot_model(model_LSTM, to_file=PATH + MODEL_IMAGE + LSTM_IMAGE, show_shapes=True, show_layer_names=True)
+
         return model_LSTM
 
     def trainModel(self):
-
+        early_stopping = EarlyStopping(monitor='val_loss', patience=STOP_PATIENCE, verbose=0, mode='min')
+        checkpoint = ModelCheckpoint(PATH + MODEL + LSTM_MODEL, save_best_only=True, monitor='val_accuracy', mode='max')
         history = self.model.fit(
             [np.array(self.train_title), np.array(self.train_text)],
             self.train_rating,
             epochs=EPOCH,
             batch_size=BATCH_SIZE,
             verbose=1,
-            validation_data=([np.array(self.val_title), np.array(self.val_text)], self.val_rating)
+            validation_data=([np.array(self.val_title), np.array(self.val_text)], self.val_rating),
+            callbacks=[early_stopping, checkpoint]
         )
 
-        self.model.save(PATH + MODEL + LSTM_MODEL)
+        # self.model.save(PATH + LSTM_MODEL)
 
         plt.figure()
         plt.plot(history.history['accuracy'], label='Train Accuracy')
         plt.plot(history.history['loss'], label='Train Loss')
-        plt.title('LSTM Model')
+        plt.title('LSTM MODEL')
         plt.ylabel('Value')
         plt.xlabel('Epoch')
         plt.legend()
@@ -86,9 +97,13 @@ class LSTM:
         plt.close()
 
     def testModel(self, x_test, y_test):
+        self.model = load_model(PATH + MODEL + LSTM_MODEL)
         y_pred = self.model.predict(x_test)
-        pred = np.argmax(y_pred,axis=1)
+        pred = np.argmax(y_pred, axis=1)
         report = classification_report(y_test, utils.to_categorical(pred, num_classes=3))
+        acc = accuracy_score(y_test, utils.to_categorical(pred, num_classes=3))
+        acc_line = f'Accuracy: {acc}\n'
+        report += acc_line
         print(report)
 
         with open(PATH + REPORT + LSTM_REPORT, 'w') as file:
