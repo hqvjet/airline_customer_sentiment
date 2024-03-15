a2673545b97935a0fa68552fd0ebf535a6e3eec3
hqvjet <viethq.1906@gmail.com>
2024-03-15 20:12:55 +0700

    fix

---
+++ client/src/constants/constants.ts
@@ -0,0 +1,65 @@
+export const GLOVE_INFO = {
+    CNN: {
+        name: 'CNN MODEL',
+        neg: 91,
+        neu: 43,
+        pos: 88
+    },
+    LSTM: {
+        name: 'LSTM MODEL',
+        neg: 90,
+        neu: 19,
+        pos: 87
+    },
+    BILSTM: {
+        name: 'BILSTM MODEL',
+        neg: 91,
+        neu: 42,
+        pos: 87
+    },
+    FUSION_CNN_BILSTM: {
+        name: 'FUSION CNN MODEL',
+        neg: 93,
+        neu: 48,
+        pos: 89
+    },
+    ENSEMBLE_CNN_BILSTM: {
+        name: 'ENSEMBLE CNN MODEL',
+        neg: 92,
+        neu: 28,
+        pos: 90
+    },
+}
+
+export const PHOBERT_INFO = {
+    CNN: {
+        name: 'CNN MODEL',
+        neg: 91,
+        neu: 43,
+        pos: 88
+    },
+    LSTM: {
+        name: 'LSTM MODEL',
+        neg: 90,
+        neu: 19,
+        pos: 87
+    },
+    BILSTM: {
+        name: 'BILSTM MODEL',
+        neg: 91,
+        neu: 42,
+        pos: 87
+    },
+    FUSION_CNN_BILSTM: {
+        name: 'FUSION CNN MODEL',
+        neg: 93,
+        neu: 48,
+        pos: 89
+    },
+    ENSEMBLE_CNN_BILSTM: {
+        name: 'ENSEMBLE CNN MODEL',
+        neg: 92,
+        neu: 28,
+        pos: 90
+    },
+}
--- client/src/modules/ModelSelectPage/index.tsx
+++ client/src/modules/ModelSelectPage/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Col, Row, Space, Card, Button, Typography } from "antd"
 import Link from 'next/link';
+import { GLOVE_INFO, PHOBERT_INFO } from '@/constants/constants'
 
 const { Title, Text } = Typography
 export default function ModelSelectPage() {
@@ -12,19 +13,54 @@ export default function ModelSelectPage() {
                 <Title><p className="text-white text-center">Global Vector Embedding</p></Title>
                 <Row className="w-full h-32 gap-5" justify={'center'}>
                     <Link href="/show-model/glove/lstm" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">LSTM MODEL</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">{GLOVE_INFO.LSTM.name}</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.LSTM.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.LSTM.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.LSTM.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                     <Link href="/show-model/glove/cnn" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">CNN MODEL</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">CNN MODEL</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.CNN.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.CNN.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.CNN.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                     <Link href="/show-model/glove/bilstm" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">BiLSTM MODEL</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">BiLSTM MODEL</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.BILSTM.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.BILSTM.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.BILSTM.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                     <Link href="/show-model/glove/ensemble_cnn_bilstm" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">ENSEMBLE CNN + BILSTM</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">ENSEMBLE CNN + BILSTM</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.ENSEMBLE_CNN_BILSTM.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.ENSEMBLE_CNN_BILSTM.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.ENSEMBLE_CNN_BILSTM.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                     <Link href="/show-model/glove/fusion_cnn_bilstm" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">FUSION CNN + BILSTM</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">FUSION CNN + BILSTM</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.FUSION_CNN_BILSTM.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.FUSION_CNN_BILSTM.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.FUSION_CNN_BILSTM.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                 </Row >
             </Col >
@@ -32,19 +68,54 @@ export default function ModelSelectPage() {
                 <Title><p className="text-white text-center">PhoBERT Embedding</p></Title>
                 <Row className="w-full h-32 gap-5" justify={'center'}>
                     <Link href="/show-model/phobert/lstm" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">LSTM MODEL</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">LSTM MODEL</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.LSTM.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.LSTM.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.LSTM.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                     <Link href="/show-model/phobert/cnn" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">CNN MODEL</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">CNN MODEL</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.CNN.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.CNN.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.CNN.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                     <Link href="/show-model/phobert/bilstm" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">BiLSTM MODEL</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">BiLSTM MODEL</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.BILSTM.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.BILSTM.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.BILSTM.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                     <Link href="/show-model/phobert/ensemble_cnn_bilstm" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">ENSEMBLE CNN + BILSTM</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">ENSEMBLE CNN + BILSTM</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.ENSEMBLE_CNN_BILSTM.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.ENSEMBLE_CNN_BILSTM.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.ENSEMBLE_CNN_BILSTM.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                     <Link href="/show-model/phobert/fusion_cnn_bilstm" className="w-1/6" passHref>
-                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" ><p className="text-white">FUSION CNN + BILSTM</p></Button>
+                        <Button size='large' type='primary' className="w-full h-full shadow shadow-white" >
+                            <p className="text-white">FUSION CNN + BILSTM</p>
+                            <Col>
+                                <p className="text-green-500">Positive: {GLOVE_INFO.FUSION_CNN_BILSTM.pos}</p>
+                                <p className="text-yellow-500">Neutral: {GLOVE_INFO.FUSION_CNN_BILSTM.neu}</p>
+                                <p className="text-red-500">Negative: {GLOVE_INFO.FUSION_CNN_BILSTM.neg}</p>
+                            </Col>
+                        </Button>
                     </Link>
                 </Row>
             </Col>
